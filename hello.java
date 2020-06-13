709f3e8a8f93eb66abe633e5f91b6529424466d7
lklavish <lklavish@gmail.com>
2020-06-13 16:44:31 +0530

    New line added

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  // It will be shown on screen.
+        System.out.println("This is my first Java code on git");
     }  
 }
