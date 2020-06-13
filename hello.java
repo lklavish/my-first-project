ca4d013fcc0b2fffdddc0dab2476ee70c8fbfc0a
lklavish <lklavish@gmail.com>
2020-06-13 16:40:18 +0530

    Comments added

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java");  // It will be shown on screen.
     }  
 }
