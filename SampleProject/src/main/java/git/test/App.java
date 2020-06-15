6d91534e734be3d050af85c6763d543dfaca8a59
Lavish Kanugo <lavishkanugo.path@gmail.com>
2020-06-15 12:33:51 +0530

    This is my first commit

---
+++ SampleProject/src/main/java/git/test/App.java
@@ -0,0 +1,13 @@
+package git.test;
+
+public class App {
+
+	public String sample()
+	{
+		return "sample";
+	}
+	
+	public static void main (String []args) {
+		System.out.println("Hello World");
+	}
+}
